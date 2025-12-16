436264043373c4172aa514bf72a023078a22a38c
Cristian Ribeiro Etchebest <65380172+etchebest@users.noreply.github.com>
2025-12-16 14:05:14 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 repo de implementação do curso http://www.tabnews.com.br para o curso http://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-
-    return <h1>Pau que nasce torto é bug de fábrica, não é feature :) </h1>
+  return <h1>Pau que nasce torto é bug de fábrica, não é feature :) </h1>;
 }
 
 export default Home;
