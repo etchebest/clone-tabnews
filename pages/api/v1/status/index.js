ee424bbdf7667a0c7e1a3b1473be00474f57f73e
Cristian Ribeiro Etchebest <cristian.etchebest@gmail.com>
2025-12-17 15:24:37 -0300

    wip/add-database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,12 @@
-function status(request, response) {
-  response.status(200).json({name:"teste"})
+import database from "../../../../infra/database.js"
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum;')
+
+  console.log(result.rows);
+
+
+  response.status(200).json({ name: "teste" })
 }
 
 export default status
